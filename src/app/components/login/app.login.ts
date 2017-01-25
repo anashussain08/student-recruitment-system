127b22ba833efe28802dcac6e08930de42120e01
anas.hussain08 <anas.hussain08@gmail.com>
2017-01-25 21:37:06 +0500

    ui updated

--- src/app/components/login/app.login.ts
+++ src/app/components/login/app.login.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 
 export class Login{
+    types:any = ['Student','Company','Admin'];
 
 }
--- src/app/components/signup/app.signup.ts
+++ src/app/components/signup/app.signup.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class SignUp{
-    
+    types:any = ['Student','Company'];
 }
